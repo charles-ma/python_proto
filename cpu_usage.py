d40cac5afece26803dda9674039e9e80e08d90b6
Charles <mcrichard.buaa@gmail.com>
2022-08-03 18:04:07 +0000

    more delicate cases

--- cpu_usage.py
+++ cpu_usage.py
@@ -6,12 +6,14 @@ def infinite_loop():
 
 '''
 if we spin up two running processes without threads, will consume 2 cpu cores
-if we spin up processes with threads, each process will still consume 100% cpu with 50% for each thread
+cpu usages of multiple threads always add up to ~100%
 '''
 if __name__ == '__main__':
-    # t1 = threading.Thread(target=infinite_loop)
-    # t2 = threading.Thread(target=infinite_loop)
+    t1 = threading.Thread(target=infinite_loop)
+    t2 = threading.Thread(target=infinite_loop)
+    t3 = threading.Thread(target=infinite_loop)
 
-    # t1.start()
-    # t2.start()
+    t1.start()
+    t2.start()
+    t3.start()
     infinite_loop()
